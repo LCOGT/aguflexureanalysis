12ebf3d2001db11ae412fa86630b7ae1a54742c0
Daniel Harbeck <drharbeck@gmail.com>
2019-11-20 14:34:21 -0800

    more deployable code

--- deploy/crawlpinholes.sh
+++ deploy/crawlpinholes.sh
@@ -5,4 +5,4 @@ ndays=3
 NCPU=2
 
 python aguanalysis/agupinholesearch.py --ndays ${ndays} -ncpu {$NCPU} --loglevel INFO --database ${database}
-python aguanalysis/aguanalysis.py --database ${database} -outputpath /home/dharbeck/public_html/agupinhole
+python aguanalysis/aguanalysis.py --database ${database} --outputpath /home/dharbeck/public_html/agupinhole
