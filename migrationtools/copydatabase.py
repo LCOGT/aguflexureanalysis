c341842997a48c43a321dc73cdb84977a88f4fb2
Daniel Harbeck <drharbeck@gmail.com>
2020-03-19 23:02:11 -0700

    code prettyfication

--- migrationtools/copydatabase.py
+++ migrationtools/copydatabase.py
@@ -9,39 +9,33 @@ import lcogt_nres_aguanalysis.agupinholedb as agupinholedb
 
 def parseCommandLine():
     parser = argparse.ArgumentParser(
-    description='Copy noisegain database from A to B')
-
+        description='Copy noisegain database from A to B')
     parser.add_argument('--loglevel', dest='log_level', default='INFO', choices=['DEBUG', 'INFO', 'WARN'],
-                    help='Set the debug level')
-
-    parser.add_argument('--inputurl', type=str,  default='sqlite:///noisegain.sqlite', help="input database")
-    parser.add_argument('--outputurl' ,type=str , default='sqlite:///noisegain.sqlite', help="input database")
-
+                        help='Set the debug level')
+    parser.add_argument('--inputurl', type=str, default='sqlite:///noisegain.sqlite', help="input database")
+    parser.add_argument('--outputurl', type=str, default='sqlite:///noisegain.sqlite', help="input database")
     args = parser.parse_args()
 
     logging.basicConfig(level=getattr(logging, args.log_level.upper()),
-                    format='%(asctime)s.%(msecs).03d %(levelname)7s: %(module)20s: %(message)s')
+                        format='%(asctime)s.%(msecs).03d %(levelname)7s: %(module)20s: %(message)s')
     return args
+
 
 if __name__ == '__main__':
     args = parseCommandLine()
-    print (f"Copy from {args.inputurl} -> {args.outputurl}")
+    print(f"Copy from {args.inputurl} -> {args.outputurl}")
 
-    input = agupinholedb.get_session (args.inputurl, Base=agupinholedb.Base_v1)
-    output = agupinholedb.get_session (args.outputurl)
+    input = agupinholedb.get_session(args.inputurl, Base=agupinholedb.Base_v1)
+    output = agupinholedb.get_session(args.outputurl)
 
+    q = input.query(agupinholedb.PinholeMeasurement_v1)
 
-    q = input.query (agupinholedb.PinholeMeasurement_v1)
-
-    print ("Found {} records to copy.".format(q.count()))
-    print ("Refactoring data")
+    print("Found {} records to copy.".format(q.count()))
+    print("Refactoring data")
     newdata = [agupinholedb.pinholefrompinhole_v1(e) for e in q.all()]
 
-    # now do anything in between.
-
-    print ("Now doing bulk insert")
-    output.bulk_save_objects (newdata)
-
+    print("Now doing bulk insert")
+    output.bulk_save_objects(newdata)
     output.commit()
 
     input.close()
