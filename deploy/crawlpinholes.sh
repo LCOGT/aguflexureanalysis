1fc080c167eebd8ec9b52bf4490e035d89d9e5d4
Daniel Harbeck <drharbeck@gmail.com>
2020-03-26 16:25:14 -0700

    update of crawler script

--- deploy/crawlpinholes.sh
+++ deploy/crawlpinholes.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#DATABASE="sqlite:///agupinholelocations.sqlite"
-
 ndays=4
 NCPU=1
 
-agupinholesearch --ndays ${ndays} --ncpu ${NCPU} --loglevel INFO --database ${DATABASE} --useaws 
+agupinholesearch --ndays ${ndays} --ncpu ${NCPU} --loglevel INFO --database ${DATABASE} --useaws
 aguanalysis --database ${DATABASE} --outputpath agupinhole_html
