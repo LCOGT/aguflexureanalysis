ccfa503b0dc01b4b3352c0ecf20761e8b72fe8c6
Daniel Harbeck <drharbeck@gmail.com>
2021-05-05 12:10:54 -0700

    add testing framework

---
+++ testing/test_pinholecentering.py
@@ -0,0 +1,23 @@
+import argparse
+from lcogt_nres_aguanalysis import agupinholesearch
+
+
+TESTDATA = {'tlv1m0XX-ak14-20210501-0127-e00.fits.fz' : {'x': 782.6, 'y': 586.5, 'error': None},
+}
+
+TESTDATADIR = 'testing/testdata'
+CENTERTOLERANCE= 2
+
+args = argparse.Namespace()
+args.makepng = False
+
+for image in TESTDATA:
+    print (image, TESTDATA[image])
+    measurement = agupinholesearch.findPinhole (f'{TESTDATADIR}/{image}', args, None)
+    x = measurement.xcenter
+    y = measurement.ycenter
+    print (f"{image} measured {x} / {y}  should be   {TESTDATA[image]['x']} / {TESTDATA[image]['x']}")
+    assert (abs(x - TESTDATA[image]['x']) < CENTERTOLERANCE)
+    assert (abs(y - TESTDATA[image]['y']) < CENTERTOLERANCE)
+
+
