68c4123b36ac1c1ed92968740df1b1494a1c5ead
Daniel Harbeck <drharbeck@gmail.com>
2021-05-11 16:30:44 -0700

    agu trend graphs to include an estimate of the current pin hole location and show CRPIX12 values to quickly see if there is a mismatch in the configured pinhole location

--- lcogt_nres_aguanalysis/aguanalysis.py
+++ lcogt_nres_aguanalysis/aguanalysis.py
@@ -99,7 +99,7 @@ def dateformat():
     """ Utility to prettify a plot with dates.
     """
     starttime = datetime.datetime(2017, 1, 1)
-    endtime = datetime.datetime.now() + datetime.timedelta(days=7)
+    endtime = datetime.datetime.now() + datetime.timedelta(days=14)
     plt.xlim([starttime, endtime])
     plt.gcf().autofmt_xdate()
     years = mdates.YearLocator()  # every year
@@ -116,31 +116,23 @@ def dateformat():
 
 
 
-def findrecentPinhole(camera='ak14', sql='sqlite:///agupinholelocations.sqlite'):
-
-    images, alts, az, xs, ys, dobs, foctemps, crpix1, crpix2 = readPinHoles(camera, sql)
-
-    _logger.info("Found {} entries".format(len(images)))
+def findrecentPinhole(dobs, xs, ys):
 
     ## Start determining the most recent pinhole location
     recent_x = recent_y = None
-    if len (dobs) > 10:
+    if len (dobs) > 11:
         lastdayobs = dobs[-1]
-        recentindex = dobs > lastdayobs - datetime.timedelta (days=7)
+        recentindex = dobs > lastdayobs - datetime.timedelta (days=14)
         if sum(recentindex) < 7:
             _logger.info ("Not enough pinhole locations measured")
             return None, None
 
         recent_x = np.median(xs[recentindex])
         recent_y = np.median(ys[recentindex])
-
-        crx = np.median (crpix1[recentindex])
-        cry = np.median (crpix2[recentindex])
-
-        _logger.info (f"Best pinhole: {recent_x} / {recent_y}, compare to CRPIX {crx} / {cry}")
-
-
-
+        return recent_x, recent_y
+    else:
+        _logger.warning("Not enough recent measurements")
+        return None, None
 
 
 def plotagutrends(camera='ak01', sql='sqlite:///agupinholelocations.sqlite', outputpath='.'):
@@ -148,29 +140,55 @@ def plotagutrends(camera='ak01', sql='sqlite:///agupinholelocations.sqlite', out
     matplotlib.rcParams['savefig.dpi'] = 300
     matplotlib.rcParams['figure.figsize'] = (8.0,6.0)
 
-    images, alts, az, xs, ys, dobs, foctemps, crpix1, crpix2 = readPinHoles(camera, sql)
+    images, alts, az, xraw, yraw, dobs, foctemps, crpix1, crpix2 = readPinHoles(camera, sql)
     _logger.info("Found {} entries".format(len(images)))
-    index = (np.isfinite(xs)) & np.isfinite(ys) & (xs != 0)  # & (alts>89)
-    xs = xs - np.nanmedian(xs[index])
-    ys = ys - np.nanmedian(ys[index])
+    # Sort out bad values
+    index = (np.isfinite(xraw)) & np.isfinite(yraw) & (xraw != 0)  # & (alts>89)
+    xraw = xraw[index]
+    yraw = yraw[index]
+    dobs = dobs[index]
+    alts= alts[index]
+    crpix1 = crpix1[index]
+    crpix2 = crpix2[index]
+    foctemps = foctemps[index]
+    az = az[index]
+
+    # Median-correct to we have a nice homogeneous plotting range between camera adjustments.
+    xraw_median = np.nanmedian(xraw)
+    yraw_median = np.nanmedian(yraw)
+    xs = xraw - xraw_median
+    ys = yraw - yraw_median
 
     timewindow = datetime.timedelta(days=5)
     filteredy = ys * 0
     filteredx = xs * 0
     smallnumber = (np.abs(ys) < 15) & (np.abs(xs) < 15)
+    recent_x, recent_y = findrecentPinhole(dobs, xraw,yraw)
+
+    print (f"recent pinhole location: {recent_x} {recent_y}, compare to CRPix: {crpix1[-1]} {crpix2[-1]}")
 
     index = (np.isfinite(xs)) & np.isfinite(ys) & (xs != 0)  # & (alts>89)
     # print("Min {} Max {} ".format(dobs[index].min(), dobs[index].max()))
     plt.subplot(211)
-    plt.plot(dobs[index], xs[index], '.', label="pinhole x",  markersize=1)
+    plt.plot(dobs, xs, '.', label="pinhole x",  markersize=1)
+    if recent_x:
+        plt.plot (dobs[-1], recent_x -xraw_median, 'o', color='green')
+    else:
+        recent_x = -1
     plt.ylim([-15, 15])
-    plt.title("%s pinhole location in focus images X" % (camera))
+    plt.title(f"{camera} pinhole location in focus images X recent {recent_x:6.1f}, CRPIX1: {crpix1[-1]:6.1f}:")
     dateformat()
 
     plt.subplot(212)
-    plt.plot(dobs[index], ys[index], '.', label="pinhole y",  markersize=1)
+    plt.plot(dobs, ys, '.', label="pinhole y",  markersize=1)
+    if recent_y:
+        plt.plot (dobs[-1], recent_y -yraw_median, 'o', color='green')
+    else:
+        recent_y = -1
+
+
     plt.ylim([-15, 15])
-    plt.title("%s pinhole location in focus images Y" % (camera))
+    plt.title(f"{camera} pinhole location in focus images Yrecent {recent_y:6.1f}, CRPIX2: {crpix2[-1]:6.1f}:")
     dateformat()
     plt.tight_layout()
 
@@ -190,25 +208,25 @@ def plotagutrends(camera='ak01', sql='sqlite:///agupinholelocations.sqlite', out
             xs[(dobs > dobs[ii] - timewindow) & (dobs < dobs[ii] + timewindow) & (smallnumber)])
 
     plt.subplot(221)
-    plt.plot(alts[index], filteredy[index] - np.nanmedian(filteredy[index]), '.', label="pinhole y",  markersize=2)
+    plt.plot(alts, filteredy - np.nanmedian(filteredy), '.', label="pinhole y",  markersize=2)
     plt.legend()
     plt.ylim([-15, 15])
     plt.xlabel('ALT')
 
     plt.subplot(222)
-    plt.plot(az[index], filteredy[index] - np.nanmedian(filteredy[index]), '.', label="pinhole y",  markersize=2)
+    plt.plot(az, filteredy - np.nanmedian(filteredy), '.', label="pinhole y",  markersize=2)
     plt.ylim([-15, 15])
     plt.legend()
     plt.xlabel('AZ')
 
     plt.subplot(223)
-    plt.plot(alts[index], filteredx[index] - np.nanmedian(filteredx[index]), '.', label="pinhole x",  markersize=2)
+    plt.plot(alts, filteredx - np.nanmedian(filteredx), '.', label="pinhole x",  markersize=2)
     plt.ylim([-15, 15])
     plt.legend()
     plt.xlabel('ALT')
 
     plt.subplot(224)
-    plt.plot(az[index], filteredx[index] - np.nanmedian(filteredx[index]), '.', label="pinhole x",  markersize=2)
+    plt.plot(az, filteredx - np.nanmedian(filteredx), '.', label="pinhole x",  markersize=2)
     plt.ylim([-15, 15])
     plt.legend()
     plt.xlabel('AZ')
@@ -224,7 +242,7 @@ def plotagutrends(camera='ak01', sql='sqlite:///agupinholelocations.sqlite', out
     plt.subplot(211)
     plt.title("%s pinhole location in focus images " % (camera))
 
-    plt.plot(foctemps[index], xs[index], ".", label="pinhole x",  markersize=2)
+    plt.plot(foctemps, xs, ".", label="pinhole x",  markersize=2)
     plt.ylabel("x-position")
     plt.xlabel("WMS temp [\deg C]")
     plt.ylim([-15, 15])
@@ -233,7 +251,7 @@ def plotagutrends(camera='ak01', sql='sqlite:///agupinholelocations.sqlite', out
     plt.subplot(212)
     plt.title("%s pinhole location in focus images " % (camera))
 
-    plt.plot(foctemps[index], ys[index], ".", label="pinhole x",  markersize=2)
+    plt.plot(foctemps, ys, ".", label="pinhole x",  markersize=2)
     plt.ylim([-15, 15])
     plt.xlim([-10, 35])
     plt.ylabel("x-position")
