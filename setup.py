b8bdf83d40daf1a024b7faf0679454644091099f
Daniel Harbeck <drhaz@users.noreply.github.com>
2023-01-18 18:36:33 -0800

    add ak17,ak18 (sigh, so many dead cams), tag new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='nresaguflexure',
-    version='1.2.2',
+    version='1.2.3',
     packages=setuptools.find_packages(),
     url='',
     author='Daniel Harbeck',
