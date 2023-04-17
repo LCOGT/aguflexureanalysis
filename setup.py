8c56103da853e0f2c86c34d51c1298826ca7baae
Daniel Harbeck <drhaz@users.noreply.github.com>
2023-04-17 14:21:33 -0700

    more cameras, version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='nresaguflexure',
-    version='1.2.3',
+    version='1.2.4',
     packages=setuptools.find_packages(),
     url='',
     author='Daniel Harbeck',
